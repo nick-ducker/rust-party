cf41cd111a2c893ca31bc872e61c1241a57ef1dc
Nick Ducker <nick_ducker@live.com>
2022-03-09 20:36:20 +1100

    scaffold api

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ get, post, web, App, HttpResponse, HttpServer, Responder };
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(ping)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
